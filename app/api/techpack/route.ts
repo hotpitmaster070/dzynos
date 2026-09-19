72e58309a4c84dcec7955c29ba5d482f9f35dc0a
hotpitmaster070 <hotpitmaster070@gmail.com>
2026-09-19 09:35:40 +0200

    Update route.ts

--- app/api/techpack/route.ts
+++ app/api/techpack/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server"
-import { calculatePatterns } from "@/lib/patternCalculator"
+import { ... } from '../../../lib/patternCalculator'
 
 export async function POST(req: Request){
   try{
